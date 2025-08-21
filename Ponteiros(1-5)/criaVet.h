20a8ca54a40d89d2166559ea190c56f0799cbf2e
PaoBatatus <pedro.lacerdalves@gmail.com>
2025-08-20 23:34:36 -0300

    Update exclude previous paste

--- Ponteiros(1-5)/criaVet.h
+++
@@ -1,5 +0,0 @@
-#ifndef CRIAVET_H
-#define CRIAVET_H
-
-void cria_vet(int *v, int n);
-#endif
--- Ponteiros(1-5)/invertVet.h
+++
@@ -1,5 +0,0 @@
-#ifndef INVERVET_H
-#define INVERVET_H
-
-void inverte_vet(int *v1,int *v2, int n);
-#endif
