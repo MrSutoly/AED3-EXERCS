8601b5d8af0aca630cfe087a183456f6499bc708
PaoBatatus <pedro.lacerdalves@gmail.com>
2025-09-28 20:06:45 -0300

    Update ListaDinEncad.h

--- Listas(1-9)/ListaDinEncad.h
+++ Listas(1-9)/ListaDinEncad.h
@@ -1,3 +1,6 @@
+#ifndef LISTADIN_H
+#define LISTADIN_H
+
 
 struct aluno{
     int matricula;
@@ -23,3 +26,10 @@ void al_mat(Lista* li, int mat);
 void al_pos(Lista* li, int pos);
 int al_rec_eq(Lista* li1, Lista* li2);
 void put_pos(Lista* li, int pos, struct aluno al);
+int Concatenate_List(Lista* list1, Lista* list2);
+void remove_duplicatas(Lista* li);
+void fusao_listas_ordenadas(Lista* l1, Lista* l2);
+int Troca(Lista* pLista, struct elemento* p);
+struct elemento* busca_por_matricula(Lista *li, int mat);
+
+#endif //LISTADIN_H
